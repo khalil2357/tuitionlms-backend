2448a3e4257907981bfa44296b3be7a5e5ddf6c5
Md Ibrahim Khalil <siamahamedab@gmail.com>
2026-05-10 13:48:28 +0600

    feat: implement user profile retrieval and update endpoints with JWT authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body,Controller,Get,Patch,Req,UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Req() req) {
+        return this.usersService.getProfile(req.user.id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('update')
+    UpdateUser(
+        @Req() req: any,
+        @Body() payload: any
+    ){
+        return this.usersService.updateUser(req.user.id, payload);
+    }
+    @UseGuards(JwtAuthGuard)
+    @Get('all')
+    getAllUsers(){
+        return this.usersService.getAllUsers();
+    }
+}

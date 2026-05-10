e31aa5b43754350a10110b7bb943ddf87aed1272
Md Ibrahim Khalil <siamahamedab@gmail.com>
2026-05-10 13:42:23 +0600

    fix: update UsersModule to import PrismaModule instead of PrismaService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UsersService],
   controllers: [UsersController]
 })
